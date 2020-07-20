d46ad6e417a25fd794d74298332df2f8ed2a7da0
Marcus Primo <marcus.primov@gmail.com>
2020-07-20 19:56:05 -0300

    Implementação axios no login

--- src/Paginas/Login.js
+++ src/Paginas/Login.js
@@ -4,11 +4,13 @@ import { Paper, withStyles, Grid, TextField, Button, Typography, InputLabel, Sel
 import purple from '@material-ui/core/colors/purple';
 import { sizing } from '@material-ui/system';
 import { Face, Fingerprint } from '@material-ui/icons'
-import usuarioApi from '../Requisições/Usuario.js'
+import Logar from '../Requisições/Usuario.js'
+import { makeStyles } from '@material-ui/core/styles';
 
-var axios = require("axios");
+// var axios = require("axios");
 
-const styles = theme => ({
+
+const useStyles = makeStyles((theme) => ({
     margin: {
         margin: theme.spacing.unit * 5.5,
     },
@@ -18,123 +20,135 @@ const styles = theme => ({
     top: {
         top: 15
     },
+    paper: {
+      marginTop: theme.spacing(10),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    form: {
+      width: '100%',
+      backgroundColor: 'rgba(0,0,0,0)',
+      paddingLeft: '12%',
+      paddingRight: '12%',
+      paddingTop: '6%',
+      padding:'1%',
+      borderRadius: 20,
+    },
+    submit: {
+      margin: theme.spacing(4, 0, 4),
+      padding: '2.5%',
+    },
+    text: {
+      margin: theme.spacing(3, 0, 0.5),
+      borderRadius: 4,
+      backgroundColor: 'whitesmoke',
+    },
+    titulo:{
+      textAlign: 'center',
+      marginBottom: '5%',
+    }
+  }));
+
+
+function Login() {
+
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
     
-});
+    
 
+    function handleInsert(event){
+        event.preventDefault();
 
-class LoginTab extends React.Component {
-
-    constructor(props){
-        super(props)
-            this.state = {
-                email: '',
-                senha: ''
-            }
-    }
-
-    handleEmailChange = (event) => {
-        this.setState({
-            email: event.target.value
-        })
-    }
-    handleSenhaChange = (event) => {
-        this.setState({
-            senha: event.target.value
-            
-        })
-        console.log(this.state.senha)
-    }
-    handleSubmit = (event) => {
-
+        setEmail('');
+        setSenha('');
         
-        axios.post("http://localhost:3001/login", {email: this.state.email, senha:this.state.senha}).then(function(data){
-            console.log(data.data);
-        }).catch(function(error){
-            console.log(error);
-        });;
+        Logar({email, senha});
+
+        alert('Parabéns ' + email + ', seu login foi realizado com sucesso!');
     }
 
-    render() {
-        const { classes } = this.props;
-        document.body.style = 'background: #26a69a;';
-       
-        return (
-            <Grid container justify="center" style={{ marginTop: '150px' }}> 
-                <Grid item xls={0} sm={4} xlg={5}/>
+    document.body.style = 'background: #26a69a;';
 
-                <Grid item xls={12} sm={4} xlg={2}>
-                    <Paper className={classes.padding}>
+    const classes = useStyles();
+    return (
+        <Grid container justify="center" style={{ marginTop: '150px' }}> 
+            <Grid item xls={0} sm={4} xlg={5}/>
 
-                      <form onSubmit = {this.handleSubmit}>
-                            <div className={classes.margin}>
+            <Grid item xls={12} sm={4} xlg={2}>
+                <Paper className={classes.paper}>
 
-                                <Grid container spacing={8} alignItems="flex-end">
-                                
-                                    <Grid item md={true} sm={true} xs={true}>
-                                        <Typography
-                                            align="center"
-                                            color="textPrimary"
-                                            variant="h4"
-                                        >
-                                            Bem vindo ao NSA
-                                        </Typography>
-                                    </Grid>
+                <form onSubmit = {handleInsert} className={classes.form}>
+                        <div className={classes.margin}>
+
+                            <Grid container spacing={8} alignItems="flex-end">
+                            
+                                <Grid item md={true} sm={true} xs={true}>
+                                    <Typography
+                                        align="center"
+                                        color="textPrimary"
+                                        variant="h4"
+                                    >
+                                        Bem vindo ao NSA
+                                    </Typography>
                                 </Grid>
-                                
-                                <Grid container spacing={8}  alignItems="flex-end">
-                                
-                                    <Grid item md={true} sm={true} xs={true}>
-                                            <InputLabel  justify="flex-start" id="txt-role">Entrar como:</InputLabel>
-                                                <Select
-                                                    autoWidth
-                                                    labelId="role-label"
-                                                    id="role"
-                                                    >
-                                                    <MenuItem value={10}>Aluno</MenuItem>
-                                                    <MenuItem value={20}>Professor</MenuItem>
-                                                    <MenuItem value={30}>Secretária</MenuItem>
-                                                </Select> 
-                                        </Grid> 
-                                    </Grid>
-                                
-                                
-                                <Grid container spacing={5} alignItems="flex-end">
-                                    <Grid item>
-                                        <Face />
-                                    </Grid>
-                                    <Grid item md={true} sm={true} xs={true}>
-                                        <TextField id="email" label="E-mail" type="email" fullWidth autoFocus required  value={this.state.email} onChange={this.handleEmailChange}/>
-                                    </Grid>
+                            </Grid>
+                            
+                            <Grid container spacing={8}  alignItems="flex-end">
+                            
+                                <Grid item md={true} sm={true} xs={true}>
+                                        <InputLabel  justify="flex-start" id="txt-role">Entrar como:</InputLabel>
+                                            <Select
+                                                autoWidth
+                                                labelId="role-label"
+                                                id="role"
+                                                >
+                                                <MenuItem value={10}>Aluno</MenuItem>
+                                                <MenuItem value={20}>Professor</MenuItem>
+                                                <MenuItem value={30}>Secretária</MenuItem>
+                                            </Select> 
+                                    </Grid> 
                                 </Grid>
-
-                                <Grid container spacing={5} alignItems="flex-end">
-                                    <Grid item>
-                                        <Fingerprint />
-                                    </Grid>
-                                    <Grid item md={true} sm={true} xs={true}>
-                                        <TextField id="senha" label="Senha" type="password" fullWidth required  value={this.state.senha} onChange={this.handleSenhaChange}/>
-                                    </Grid>
+                            
+                            
+                            <Grid container spacing={5} alignItems="flex-end">
+                                <Grid item>
+                                    <Face />
                                 </Grid>
-
-                                <Grid container spacing={10} alignItems="flex-end"><Grid item></Grid></Grid>
-                                <Grid container spacing={5} alignItems="flex-end"><Grid item></Grid></Grid>
-
-                                <Grid container justify="flex-end" style={{ marginTop: '10px' }}>
-                                    <Button type="submit" fullWidth variant="contained" color="primary" style={{ textTransform: "none" } }>Login</Button>
+                                <Grid item md={true} sm={true} xs={true}>
+                                    <TextField id="email" label="E-mail" type="email" fullWidth autoFocus required  value={email} onChange={e => setEmail(e.target.value)}/>
                                 </Grid>
+                            </Grid>
 
-                                <Grid container spacing={10} alignItems="flex-end"><Grid item></Grid></Grid>
+                            <Grid container spacing={5} alignItems="flex-end">
+                                <Grid item>
+                                    <Fingerprint />
+                                </Grid>
+                                <Grid item md={true} sm={true} xs={true}>
+                                    <TextField id="senha" label="Senha" type="password" fullWidth required  value={senha} onChange={e => setSenha(e.target.value)}/>
+                                </Grid>
+                            </Grid>
 
-                            </div>
-                        </form>
-                    </Paper>
+                            <Grid container spacing={10} alignItems="flex-end"><Grid item></Grid></Grid>
+                            <Grid container spacing={5} alignItems="flex-end"><Grid item></Grid></Grid>
 
-                </Grid>
-                
-                <Grid item xls={0} sm={4} xlg={5}/>
+                            <Grid container justify="flex-end" style={{ marginTop: '10px' }}>
+                                <Button type="submit" fullWidth variant="contained" color="primary" style={{ textTransform: "none" } }>Login</Button>
+                            </Grid>
+
+                            <Grid container spacing={10} alignItems="flex-end"><Grid item></Grid></Grid>
+
+                        </div>
+                    </form>
+                </Paper>
+
             </Grid>
-        );
-    }
+            
+            <Grid item xls={0} sm={4} xlg={5}/>
+        </Grid>
+    );
 }
 
-export default withStyles(styles)(LoginTab);
+
+export default Login;
--- src/Requisições/Usuario.js
+++ src/Requisições/Usuario.js
@@ -3,36 +3,12 @@ var axios = require("axios");
 function Login({email, senha}){
 
     console.log(email, senha);
-    axios.post("http://localhost:8080/login", {email: email, senha: senha}).then(function(data){
+    axios.post("http://localhost:3001/login", {email: email, senha: senha}).then(function(data){
         console.log(data.data);
+        localStorage.setItem('token', data.data.token)
      }).catch(function(error){
          console.log(error);
      });
 }
-
-// function Cadastrar({email, senha}){
-
-//     console.log(email, senha);
-
-//     axios.post("https://pw3-etec-projeto-backend.herokuapp.com/usuario", {email: email, senha:senha, roles:[]}).then(function(data){
-//         console.log(data.data);
-//      }).catch(function(error){
-//          console.log(error);
-//      });;
-// // }
-// event.preventDefault();
-// console.log(this.state.email, this.state.senha);
-// axios.post("http://localhost:3001/login", 
-// {
-//     data:
-//         {
-//             email: this.state.email,
-//             senha: this.state.senha,
-//         }
-// }, {headers: {"Access-Control-Allow-Origin": "*",  'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS', 'Content-Type': 'application/x-www-form-urlencoded'}}).then(function(data){
-//     console.log(data.data);
-//  }).catch(function(error){
-//      console.log(error);
-//  });
 
 export default Login;
