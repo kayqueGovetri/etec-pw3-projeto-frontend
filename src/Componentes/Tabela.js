8b82f056a7a9e78ab0c68b40d2346f4d0fa8782b
govetriKayque <govetrikayque@gmail.com>
2020-07-22 21:40:45 -0300

    Feito a criaçao da tabela e da listagem de aluno fake.

---
+++ src/Componentes/Tabela.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+import Moment from 'moment'
+import { 
+    Table, 
+    TableBody, 
+    TableCell, 
+    TableContainer, 
+    TableHead, 
+    TableRow, 
+    Paper
+} from '@material-ui/core'
+
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const Tabela = ({ linhas, celulas, informacaoTabela, width }) => {
+    const useStyles = makeStyles({
+        root: {
+          '&':{
+            width,
+          }
+        }
+    })
+    const classes = useStyles();
+  return (
+    <TableContainer component={Paper} className={classes.root}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {celulas.map((celula, index) => {
+                if(index === 0) return <TableCell>{celula}</TableCell>
+                return <TableCell align="right">{celula}</TableCell>
+            })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {informacaoTabela === 'Aluno' && linhas.map((linha) => (
+                <TableRow key={linha._id}>
+                <TableCell component="th" scope="row">
+                    {linha.nome}
+                </TableCell>
+                <TableCell align="right">{linha.rm}</TableCell>
+                <TableCell align="right">{linha.telefone}</TableCell>
+                <TableCell align="right">{Moment(linha.dataNascimento).format('DD/MM/YYYY')}</TableCell>
+                <TableCell align="right">{Moment(linha.dataMatricula).format('DD/MM/YYYY')}</TableCell>
+                <TableCell align="right">{linha.cep}</TableCell>
+                <TableCell align="right">{linha.cpfResponsavel}</TableCell>
+                <TableCell align="right">{linha.email}</TableCell>
+                {/* <TableCell align="right">{linha.turma.curso}</TableCell>
+                <TableCell align="right">{linha.turma.semestre}</TableCell> */}
+                </TableRow>
+            ))}
+
+            {informacaoTabela === "Turma" && linhas.map((linha) => (
+                <TableRow key={linha._id}>
+                <TableCell component="th" scope="row">
+                    {linha.curso}
+                </TableCell>
+                <TableCell align="right">{linha.semestre}</TableCell>
+                </TableRow>
+            ))}
+
+            {informacaoTabela === "Aula" && linhas.map((linha) => (
+                <TableRow key={linha._id}>
+                    <TableCell component="th" scope="row">
+                        {linha.cargaHoraria}
+                    </TableCell>
+                    <TableCell align="right">{linha.professor.nome}</TableCell>
+                    <TableCell align="right">{linha.materia.nome}</TableCell>
+                </TableRow>
+            ))}
+
+            {informacaoTabela === "Nota" && linhas.map((linha) => (
+                <TableRow key={linha._id}>
+                    <TableCell component="th" scope="row">
+                        {linha.aluno.nome}
+                    </TableCell>
+                    <TableCell align="right">{linha.aluno.rm}</TableCell>
+                    <TableCell align="right">{linha.materia.nome}</TableCell>
+                    <TableCell align="right">{linha.professor.nome}</TableCell>
+                    <TableCell align="right">{linha.materia.nome}</TableCell>
+                    <TableCell align="right">{linha.nota}</TableCell>
+                </TableRow>
+            ))}
+
+            {informacaoTabela === "Professor" && linhas.map((linha) => (
+                <TableRow key={linha._id}>
+                    <TableCell component="th" scope="row">
+                        {linha.nome}
+                    </TableCell>
+                    <TableCell align="right">{linha.dataNascimento}</TableCell>
+                    <TableCell align="right">{linha.email}</TableCell>
+                    <TableCell align="right">{linha.especialidade}</TableCell>
+                </TableRow>
+            ))}
+
+            {informacaoTabela === "Usuario" && linhas.map((linha) => (
+                <TableRow key={linha._id}>
+                    <TableCell component="th" scope="row">
+                        {linha.nome}
+                    </TableCell>
+                    <TableCell align="right">{linha.dataNascimento}</TableCell>
+                    <TableCell align="right">{linha.email}</TableCell>
+                    <TableCell align="right">{linha.especialidade}</TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+    </Table>
+</TableContainer>
+);
+}
+
+export default Tabela
+
+Tabela.propTypes = {
+    celulas: PropTypes.array.isRequired,
+    linhas: PropTypes.array.isRequired,
+    informacaoTabela: PropTypes.string.isRequired,
+    root: PropTypes.string.isRequired
+}
---
+++ src/Paginas/ListagemAlunos.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+// import { makeStyles } from '@material-ui/core/styles';
+
+// import {
+//   Button, Grid, Typography,
+// } from '@material-ui/core'
+
+import Tabela from '../Componentes/Tabela'
+
+const celulas = [
+  'nome',
+  'rm',
+  'telefone',
+  'dataNascimento',
+  'dataMatricula',
+  'cep',
+  'cpfResponsavel',
+  'email',
+  // 'curso',
+  // 'semestre'
+]
+const linhas = [
+  {
+    "telefone": "4444-4444",
+    "rm": 19584,
+    "dataNascimento": "1999-05-18T16:00:00.000Z",
+    "dataMatricula": "2020-05-18T16:00:00.000Z",
+    "cep": "06545-999",
+    "cpfResponsavel": "19856371896",
+    "email": "email@email.com",
+    "senha": "321654",
+    "_id": "5f05057d8c6aa23590127881",
+    "_turma": "5f04f8da5e29e554d83b4468",
+    "nome": "Marcus",
+    "createdAt": "2020-07-07T23:30:05.291Z",
+    "updatedAt": "2020-07-07T23:30:05.291Z",
+    "__v": 0
+  },
+]
+const ListagemAlunos = () => {
+    return(
+        <>
+          <Tabela celulas={celulas} linhas={linhas} informacaoTabela="Aluno" width="50%"/>
+        </>
+    )
+}
+
+export default ListagemAlunos;
