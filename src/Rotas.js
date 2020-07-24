e1427fe1bb8165b6d05a54ee49d14fc02eae37f7
Yuukilari <larissaaguiar80@gmail.com>
2020-07-23 23:50:00 -0300

    Mudanças Menu.

--- src/Rotas.js
+++ src/Rotas.js
@@ -8,14 +8,14 @@ import {
 import App from "../src/Paginas/App"
 import CadastroAluno from "../src/Paginas/CadastroAluno"
 import ListagemAlunos from "../src/Paginas/ListagemAlunos"
-
+import SideMenu from "../src/Paginas/SideMenu"
 
 
 const Rotas = () => (
   <>
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact={true} component={App} />
+        <Route path="/" exact={true} component={SideMenu} />
         <Route path="/aluno/cadastro" component={CadastroAluno} />
         <Route path="/aluno/listagem" component={ListagemAlunos} />
       </Switch>
