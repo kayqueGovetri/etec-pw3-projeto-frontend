680dddc9c0ec103b9890cc024de641ca662c2b3a
govetriKayque <govetrikayque@gmail.com>
2020-07-24 14:09:28 -0300

    comiitando

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'src', 'index.js'));
 });
 app.listen(port);
