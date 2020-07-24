21a4876a74f85297baf2216153021e1e1732763f
govetriKayque <govetrikayque@gmail.com>
2020-07-24 13:58:51 -0300

    It's Aliveeeee

---
+++ build/precache-manifest.57e46be57b48ce8d031d7069ffa6c079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a1a286a3ee4cf89daa2241ae1ec24c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7184ff3a2226bd118750",
+    "url": "/static/css/main.e746a0c9.chunk.css"
+  },
+  {
+    "revision": "7947baa10ad195a28708",
+    "url": "/static/js/2.e3e04d2a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e3e04d2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7184ff3a2226bd118750",
+    "url": "/static/js/main.1ba40808.chunk.js"
+  },
+  {
+    "revision": "59ca98cb257342a7f42d",
+    "url": "/static/js/runtime-main.c55b7ca1.js"
+  }
+]);
